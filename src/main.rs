0cf5a2486e476cbe6b1e6b65aa480c24c256d1a3
rink1969 <rink1969@gmail.com>
2020-04-01 18:46:08 +0800

    adjust subcommand

--- src/main.rs
+++ src/main.rs
@@ -13,10 +13,13 @@
 // limitations under the License.
 
 use clap::Clap;
-use log::info;
 use git_version::git_version;
+use log::info;
 
-const GIT_VERSION: &str = git_version!(args = ["--tags", "--always", "--dirty=-modified"], fallback = "unknown");
+const GIT_VERSION: &str = git_version!(
+    args = ["--tags", "--always", "--dirty=-modified"],
+    fallback = "unknown"
+);
 const GIT_HOMEPAGE: &str = "https://github.com/rink1969/cita_ng_template";
 
 /// This doc string acts as a help message when the user runs '--help'
@@ -24,11 +27,6 @@ const GIT_HOMEPAGE: &str = "https://github.com/rink1969/cita_ng_template";
 #[derive(Clap)]
 #[clap(version = "0.1.0", author = "Rivtower Technologies.")]
 struct Opts {
-    /// Sets a custom config file. Could have been an Option<T> with no default too
-    #[clap(short = "c", long = "config", default_value = "default.conf")]
-    config: String,
-    /// Some input. Because this isn't an Option<T> it's required to be used
-    input: String,
     #[clap(subcommand)]
     subcmd: SubCommand,
 }
@@ -38,32 +36,27 @@ enum SubCommand {
     /// print information from git
     #[clap(name = "git")]
     GitInfo,
-    /// A help message for the Test subcommand
-    #[clap(name = "test", version = "1.3", author = "Someone Else")]
-    Test(Test),
+    /// run this service
+    #[clap(name = "run")]
+    Run(RunOpts),
 }
 
-/// A subcommand for controlling testing
+/// A subcommand for run
 #[derive(Clap)]
-struct Test {
-    /// Print debug info
-    #[clap(short = "d")]
-    debug: bool,
+struct RunOpts {
+    /// Sets a custom config file. Could have been an Option<T> with no default too
+    #[clap(short = "c", long = "config", default_value = "default.conf")]
+    config: String,
+    /// Some input. Because this isn't an Option<T> it's required to be used
+    input: String,
 }
 
 fn main() {
     ::std::env::set_var("RUST_BACKTRACE", "full");
 
     log4rs::init_file("template-log4rs.yaml", Default::default()).unwrap();
-    for _ in 0..10000u64 {
-        info!("Starting...");
-    }
 
     let opts: Opts = Opts::parse();
-
-    // Gets a value for config if supplied by user, or defaults to "default.conf"
-    println!("Value for config: {}", opts.config);
-    println!("Using input file: {}", opts.input);
 
     // You can handle information about subcommands by requesting their matches by name
     // (as below), requesting just the name used, or both at the same time
@@ -71,17 +64,16 @@ fn main() {
         SubCommand::GitInfo => {
             println!("git version: {}", GIT_VERSION);
             println!("homepage: {}", GIT_HOMEPAGE);
-            std::process::exit(0);
-        },
-        SubCommand::Test(t) => {
-            if t.debug {
-                println!("Printing debug info...");
-            } else {
-                println!("Printing normally...");
-            }
+        }
+        SubCommand::Run(opts) => {
+            println!("Value for config: {}", opts.config);
+            println!("Using input file: {}", opts.input);
+            run();
         }
     }
+}
 
+fn run() {
     // more program logic goes here...
-    println!("...");
+    info!("...");
 }
